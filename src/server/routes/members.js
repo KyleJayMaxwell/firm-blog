9b2716d31c4001a5372fbc567de79ecd1a26e245
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-03-21 19:57:54 -0600

    setting up environment. knex mainly. Also routes for members and articles.

---
+++ src/server/routes/members.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/thefirm';
+var knex = require('../../../db/knex');
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Members' });
+});
+
+module.exports = router;
